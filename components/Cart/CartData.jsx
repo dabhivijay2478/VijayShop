6313b1519f26a00ac31ac932aff852b870c8319b
vijaydabhi <dabhivijay2478@gmail.com>
2023-06-10 00:14:03 +0530

    update cart

--- components/Cart/CartData.jsx
+++ components/Cart/CartData.jsx
@@ -15,55 +15,59 @@ export default function CartData() {
         }
     };
 
-    return (
-        <View style={styles.carouselItem}>
-            <Card style={styles.card}>
-                <View style={styles.container}>
+    const data = [
+        { id: 1, title: 'Card 1' },
+        { id: 2, title: 'Card 2' },
+        { id: 3, title: 'Card 3' },
+    ];
 
-                        <Card.Cover
-                        source={{
-                            uri:
-                                'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_500,h_500/global/379988/03/sv01/fnd/IND/fmt/png/Scorch-Runner-V2-Men',
-                        }}
-                        style={styles.imageStyle}
-                    />
-                    <Card.Content style={styles.contentContainer}>
-                        <Title style={styles.title}>Vijay</Title>
-                        <Paragraph style={styles.paragraph}>Card content</Paragraph>
-                    </Card.Content>
+    return (
+        <>
+            {data.map((item) => (
+                <View style={styles.carouselItem} key={item.id}>
+                    <Card style={styles.card}>
+                        <View style={styles.container}>
+
+                            <Card.Cover
+                                source={{
+                                    uri:
+                                        'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_500,h_500/global/379988/03/sv01/fnd/IND/fmt/png/Scorch-Runner-V2-Men',
+                                }}
+                                style={styles.imageStyle}
+                            />
+                            <Card.Content style={styles.contentContainer}>
+                                <Title style={styles.title}>{item.title}</Title>
+                                <Paragraph style={styles.paragraph}>Card content</Paragraph>
+                            </Card.Content>
+                        </View>
+                        <Card.Actions style={styles.cardActions}>
+                            <View style={styles.quantityContainer}>
+                                <Button
+                                    icon="minus"
+                                    onPress={handleDecrease}
+                                    mode="contained"
+                                    style={styles.quantityButton}
+                                    labelStyle={styles.buttonLabel}
+                                >
+                                    -
+                                </Button>
+                                <Text style={styles.quantityText}>{quantity}</Text>
+                                <Button
+                                    icon="plus"
+                                    onPress={handleIncrease}
+                                    mode="contained"
+                                    style={styles.quantityButton}
+                                    labelStyle={styles.buttonLabel}
+                                >
+                                    +
+                                </Button>
+                            </View>
+
+                        </Card.Actions>
+                    </Card>
                 </View>
-                <Card.Actions style={styles.cardActions}>
-                    <View style={styles.quantityContainer}>
-                        <Button
-                            icon="minus"
-                            onPress={handleDecrease}
-                            mode="contained"
-                            style={styles.quantityButton}
-                            labelStyle={styles.buttonLabel}
-                        >
-                            -
-                        </Button>
-                        <Text style={styles.quantityText}>{quantity}</Text>
-                        <Button
-                            icon="plus"
-                            onPress={handleIncrease}
-                            mode="contained"
-                            style={styles.quantityButton}
-                            labelStyle={styles.buttonLabel}
-                        >
-                            +
-                        </Button>
-                    </View>
-                    {/* <Button
-                        mode="contained"
-                        style={styles.button}
-                        labelStyle={styles.buttonLabel}
-                    >
-                        Shop Now
-                    </Button> */}
-                </Card.Actions>
-            </Card>
-        </View>
+            ))}
+        </>
     );
 }
 
@@ -84,17 +88,16 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
     },
     imageStyle: {
-
-        alignSelf: 'flex-start',
-        justifyContent: 'flex-start',
+        alignSelf: 'flex-end',
+        justifyContent: 'flex-end',
         height: 100,
         width: 200,
     },
     contentContainer: {
         flex: 1,
-        alignSelf: 'flex-end',
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
+        alignSelf: 'flex-start',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
         marginLeft: 10,
     },
     title: {
